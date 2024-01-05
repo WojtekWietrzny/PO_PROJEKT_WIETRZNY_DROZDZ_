0bd5038edf06f8ac559dacb5b46ca641f4e129e6
WojtekWietrzny <wietrzny@student.agh.edu.pl>
2024-01-05 03:17:12 +0100

    AbstractWorldMap in the making

--- Projekt_obiektowe_2024_drozdz_wietrzny/src/main/java/agh/ics/oop/model/AbstractWorldMap.java
+++ Projekt_obiektowe_2024_drozdz_wietrzny/src/main/java/agh/ics/oop/model/AbstractWorldMap.java
@@ -1,5 +1,44 @@
 package agh.ics.oop.model;
 
-public class AbstractWorldMap {
+import java.util.*;
+
+public abstract class AbstractWorldMap implements WorldMap{
+
+
+    private ArrayList<Boundary> jungles = new ArrayList<>();
+    private Map<Vector2d,Animal> animals = new HashMap<>();
+    //private Map<Vector2d,Plant> plants = new HashMap<>();
+    private int animalsQuantity = 0;
+
+
+    public void move(Animal animal){
+        for (int i = 0; i < animalsQuantity; i++){
+
+        }
+    }
+
+    public void place(Animal animal) {
+        if(canMoveTo(animal.getPosition())){
+            animals.put(animal.getPosition(), animal);
+            animalsQuantity += 1;
+        }
+    }
+
+    public void removeDead(){
+
+
+    }
+    public void eat(){
+
+    }
+    public void reproduce(){
+
+    }
+    public void growPlants(){
+
+    }
+
+    @Override
+    public abstract List<Animal> getOrderedAnimals(List<Animal> animals_listed);
 
 }
